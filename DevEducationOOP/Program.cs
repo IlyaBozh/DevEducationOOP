fbdc067dd8c394aec2622a5ddd14ed8d2f20950e
Ilya Bozhkov <ilia.il.27@mail.ru>
2022-03-05 21:50:20 +0300

    Created methods HW 1

--- DevEducationOOP/Program.cs
+++ DevEducationOOP/Program.cs
@@ -1,6 +1,10 @@
+<<<<<<< Updated upstream
 ﻿using DevEducationOOP;
 string str1 = "dasfgw";
 string str2 = "dasfgwasfryhyrdyrsry";
 Variables.SwapLines(ref str1, ref str2);
 
 Console.WriteLine($"{str1}\n{str2}");
+=======
+﻿Console.WriteLine("Hello, World!");
+>>>>>>> Stashed changes
